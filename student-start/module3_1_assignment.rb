c3b1652986dc58af22e86defe835ea1477be98c1
Zabrovskj <zabrovskj@gmail.com>
2016-02-07 05:39:19 +0500

    Решениое задание

--- student-start/module3_1_assignment.rb
+++ student-start/module3_1_assignment.rb
@@ -1,5 +1,14 @@
 require 'httparty'
 
 class Recipe
+	include HTTParty
+	base_uri 'http://food2fork.com/api'
+	default_params :key => ENV["FOOD2FORK_KEY"]
+	debug_output $stdout
+	format :json
+
+	def self.for term
+		get("/search", query: { q: term })['recipes']
+	end
 
 end
